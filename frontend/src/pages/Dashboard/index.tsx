30d580469e1c1c828e5c765e01a667209941b03b
Renato Andrade <renatorobot@outlook.com.br>
2021-11-07 18:14:37 -0300

    Routes

---
+++ frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,37 @@
+import NavBar from 'components/NavBar';
+import Footer from 'components/Footer';
+import DataTable from 'components/DataTable';
+import BarChart from 'components/BarChart';
+import DonutChart from 'components/DonutChart';
+
+function Dashboard() {
+    return (
+        <>
+        <NavBar/>
+        <div className="container">
+  
+          <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+  
+          <div className="row px-1">
+            <div className="col-sm-6">
+              <h5 className="text-center text-secondary">Taxa de Sucesso %</h5>
+              <BarChart/>
+            </div>
+            <div className="col-sm-6">
+              <h5 className="text-center text-secondary">Taxa de Sucesso %</h5>
+              <DonutChart/>
+            </div>
+          </div>
+  
+          <div className="py-2">
+            <h2 className="text-primary">Todas as vendas</h2>
+          </div>
+  
+          <DataTable/>
+        </div>
+        <Footer/>
+      </>
+    );
+  }
+  
+  export default Dashboard;
